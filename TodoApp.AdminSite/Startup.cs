5d31f5e2e21b012ca1a57cc76c02fd25bd9e8925
MartinBodocky <bodocky@gmail.com>
2015-02-13 14:14:43 +0000

    Init project structure

---
+++ TodoApp.AdminSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TodoApp.AdminSite.Startup))]
+namespace TodoApp.AdminSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ TodoApp.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TodoApp.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
